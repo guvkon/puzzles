dc61e840f303610b01685da70c9b82e5a7e653ec
Konstantin Gusev <guvkon.net@icloud.com>
2021-12-02 23:26:25 +0700

    refactor aoc 2021 day 2

--- adventofcode/2021/day2/src/Main.elm
+++ adventofcode/2021/day2/src/Main.elm
@@ -4,7 +4,6 @@ import Browser
 import Html exposing (Html, Attribute, div, textarea, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
-import ParseInt exposing (parseInt, toRadix)
 
 
 -- MAIN
@@ -24,79 +23,12 @@ type alias Model =
   }
 
 
-defaultContent = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"
-
-
 init : Model
 init =
   { input = parseInput defaultContent
   , content = defaultContent
   }
 
-
-type alias Position =
-    { horizontal : Int
-    , depth : Int
-    , aim : Int
-    }
-
-
-type Move = Forward Int | Down Int | Up Int | Stay
-
-
-stringToMove : String -> Move
-stringToMove str =
-    case String.words str of
-        x :: y :: xs -> case x of
-            "forward" -> Forward (parseIntAlways y)
-            "down" -> Down (parseIntAlways y)
-            "up" -> Up (parseIntAlways y)
-            _ -> Stay
-        x :: xs -> Stay
-        [] -> Stay
-
-
-doMove : Move -> Position -> Position
-doMove move position =
-    case position of
-        { horizontal, depth, aim } -> case move of
-            Forward value -> { position | horizontal = horizontal + value }
-            Down value -> { position | depth = depth + value }
-            Up value -> { position | depth = depth - value }
-            Stay -> position
-
-
-
-doNewMove : Move -> Position -> Position
-doNewMove move position =
-    case position of
-        { horizontal, depth, aim } -> case move of
-            Forward value -> { position | horizontal = horizontal + value, depth = depth + aim * value }
-            Down value -> { position | aim = aim + value }
-            Up value -> { position | aim = aim - value }
-            Stay -> position
-
-
-doMoves : List Move -> Position -> Position
-doMoves moves pos =
-    List.foldl doMove pos moves
-
-
-doNewMoves : List Move -> Position -> Position
-doNewMoves moves pos =
-    List.foldl doNewMove pos moves
-
-
-solution : List Move -> Int
-solution input =
-    case doMoves input { horizontal = 0, depth = 0, aim = 0 } of
-        { horizontal, depth, aim } -> horizontal * depth
-
-
-solution2 : List Move -> Int
-solution2 input =
-    case doNewMoves input { horizontal = 0, depth = 0, aim = 0 } of
-        { horizontal, depth, aim } -> horizontal * depth
 
 
 -- UPDATE
@@ -113,26 +45,6 @@ update msg model =
       { model | content = newContent, input = parseInput newContent }
 
 
-parseInput : String -> List Move
-parseInput str =
-    List.map stringToMove (String.lines str)
-
-
-isPositive : Int -> Bool
-isPositive val =
-    if val > 0 then
-        True
-    else
-        False
-
-
-parseIntAlways : String -> Int
-parseIntAlways str =
-    case parseInt str of
-        Ok val -> val
-        Err _ -> 0
-
-
 
 -- VIEW
 
@@ -140,10 +52,90 @@ parseIntAlways str =
 view : Model -> Html Msg
 view model =
   div []
-    [ textarea [ placeholder "Input", value model.content, onInput Change, rows 40, cols 100 ] []
-    , div [] [ text ("Input size: " ++ String.fromInt (List.length model.input)) ]
-    , div [] [ text ("Solution 1: " ++ String.fromInt (solution model.input)) ]
-    , div [] [ text ("Solution 2: " ++ String.fromInt (solution2 model.input)) ]
+    [ textarea [ placeholder "Input", value model.content, onInput Change, rows 20, cols 40, class "bg-dark text-light border-1 border-secondary p-2" ] []
+    , div [] [ text ( "Input size: " ++ String.fromInt ( List.length model.input ) ) ]
+    , div [] [ text ( "Solution 1: " ++ String.fromInt ( solution1 model ) ) ]
+    , div [] [ text ( "Solution 2: " ++ String.fromInt ( solution2 model ) ) ]
     ]
+
+
+
+-- LOGIC
+
+
+defaultContent = "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2"
+
+
+type alias Position =
+    { horizontal : Int
+    , depth : Int
+    , aim : Int
+    }
+
+
+type Move = Forward Int | Down Int | Up Int
+
+
+parseInput : String -> List Move
+parseInput str =
+    List.filterMap parseString (String.lines str)
+
+
+parseString : String -> Maybe Move
+parseString str =
+    case String.words str of
+        x :: y :: [] -> case x of
+            "forward" -> case String.toInt y of
+                Just val -> Just (Forward val)
+                Nothing -> Nothing
+            "down" -> case String.toInt y of
+                Just val -> Just (Down val)
+                Nothing -> Nothing
+            "up" -> case String.toInt y of
+                Just val -> Just (Up val)
+                Nothing -> Nothing
+            _ -> Nothing
+        _ -> Nothing
+
+
+solution1 : Model -> Int
+solution1 { input } =
+    case doMoves input { horizontal = 0, depth = 0, aim = 0 } of
+        { horizontal, depth, aim } -> horizontal * depth
+
+
+solution2 : Model -> Int
+solution2 { input } =
+    case doNewMoves input { horizontal = 0, depth = 0, aim = 0 } of
+        { horizontal, depth, aim } -> horizontal * depth
+
+
+doMove : Move -> Position -> Position
+doMove move position =
+    case position of
+        { horizontal, depth, aim } -> case move of
+            Forward value -> { position | horizontal = horizontal + value }
+            Down value -> { position | depth = depth + value }
+            Up value -> { position | depth = depth - value }
+
+
+
+doNewMove : Move -> Position -> Position
+doNewMove move position =
+    case position of
+        { horizontal, depth, aim } -> case move of
+            Forward value -> { position | horizontal = horizontal + value, depth = depth + aim * value }
+            Down value -> { position | aim = aim + value }
+            Up value -> { position | aim = aim - value }
+
+
+doMoves : List Move -> Position -> Position
+doMoves moves pos =
+    List.foldl doMove pos moves
+
+
+doNewMoves : List Move -> Position -> Position
+doNewMoves moves pos =
+    List.foldl doNewMove pos moves
 
 
