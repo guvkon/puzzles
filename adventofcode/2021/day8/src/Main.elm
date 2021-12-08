d9b5d4e3d2186f72de67c069c268be3640ccee56
Konstantin Gusev <guvkon.net@icloud.com>
2021-12-08 12:25:19 +0700

    solve AoC 2021 8-1

---
+++ adventofcode/2021/day8/src/Main.elm
@@ -0,0 +1,181 @@
+module Main exposing (..)
+
+import Basics
+import Browser
+import Html exposing (Html, Attribute, div, a, textarea, text)
+import Html.Attributes exposing (class, cols, href, placeholder, rows, target, value)
+import Html.Events exposing (onInput)
+import Matrix
+import Parser exposing ((|.), (|=), Parser, int, spaces, succeed, symbol)
+import Utils
+import List.Extra
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { input : Input
+  , content : String
+  }
+
+
+init : Model
+init =
+  { input = parseInput defaultContent
+  , content = defaultContent
+  }
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Change String
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Change newContent ->
+      { model | content = newContent, input = parseInput newContent }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ textarea [ placeholder "Input"
+               , value model.content
+               , onInput Change
+               , rows 20
+               , cols 90
+               , class "bg-dark text-white-50 border-1 border-secondary p-2"
+               ] []
+    , div [] [ a [ href (linkToInput 2021 8)
+             , target "_blank"
+             , class "text-white-50"
+             ] [ text "Link to puzzle's input" ] ]
+    , div [] [ text ( "Input: " ++ viewModel model ) ]
+    , div [] [ text ( "Solution 1: " ++ viewSolution ( solution1 model.input ) ) ]
+    , div [] [ text ( "Test 1: " ++ testSolution 26 ( solution1 (parseInput defaultContent) ) ) ]
+    , div [] [ text ( "Solution 2: " ++ viewSolution ( solution2 model.input ) ) ]
+    , div [] [ text ( "Test 2: " ++ testSolution 26 ( solution2 (parseInput defaultContent) ) ) ]
+    ]
+
+
+viewModel : Model -> String
+viewModel model =
+    (++) "amount of crabs = "
+        <| (String.fromInt (List.length model.input))
+
+
+viewSolution : Maybe Int -> String
+viewSolution solution =
+    case solution of
+        Just val ->
+            String.fromInt val
+        Nothing ->
+            "NaN"
+
+
+testSolution : Int -> Maybe Int -> String
+testSolution target result =
+    case result of
+        Nothing ->
+            "Error"
+        Just val ->
+            if val == target then
+                "Passing"
+            else
+                "Error"
+
+
+linkToInput : Int -> Int -> String
+linkToInput year day =
+    "https://adventofcode.com/"
+        ++ (String.fromInt year)
+        ++ "/day/"
+        ++ (String.fromInt day)
+        ++ "/input"
+
+
+
+-- LOGIC
+
+
+type alias Input = List (List String, List String)
+
+
+defaultContent =
+    """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"""
+
+
+parseInput : String -> Input
+parseInput string =
+    let
+        parseSection str =
+            String.words str
+        parseLine str =
+            String.split " | " str
+                |> List.map parseSection
+        convertToTuple strings =
+            ( Maybe.withDefault [] (Utils.element 0 strings)
+            , Maybe.withDefault [] (Utils.element 1 strings)
+            )
+    in
+    String.trim string
+        |> String.lines
+            |> List.map parseLine
+                |> List.map convertToTuple
+
+
+solution1 : Input -> Maybe Int
+solution1 input =
+    let
+        match : String -> Bool
+        match str =
+            case String.length str of
+                2 -> True
+                4 -> True
+                3 -> True
+                7 -> True
+                _ -> False
+        count : (List String, List String) -> Int
+        count pair =
+            case pair of
+                (left, right) ->
+                    right
+                        |> List.foldl (Utils.counter match) 0
+    in
+    input
+        |> List.foldl (\pair sum -> sum + count pair) 0
+        |> Just
+
+
+solution2 : Input -> Maybe Int
+solution2 input =
+    Nothing
+
+
