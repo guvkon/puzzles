735e44434b2fdd18322b7a2ccd0a462a57d9145b
Konstantin Gusev <guvkon.net@icloud.com>
2022-12-10 23:12:34 +0400

    chore: add template for AoC 2022 day 11

---
+++ adventofcode/2022/day11/solution.py
@@ -0,0 +1,91 @@
+from __future__ import annotations
+
+import re
+from dataclasses import dataclass
+from enum import Enum
+from typing import List, Optional, Tuple, Union, Dict, Set
+import numpy as np
+
+
+# === Useful Functions === #
+
+
+def splitlines(data: str, func=lambda x: x) -> List[str]:
+    return [func(line) for line in data.splitlines() if line]
+
+
+@dataclass
+class Input:
+    pass
+
+
+# === Input parsing === #
+
+
+def parse_input(data: str) -> Input:
+    lines = splitlines(data)
+    return Input()
+
+
+def parse_input1(data: str) -> Input:
+    return parse_input(data)
+
+
+def parse_input2(data: str) -> Input:
+    return parse_input(data)
+
+
+# === Solutions === #
+
+
+def solve1(input: Input) -> Optional[int]:
+    return None
+
+
+def solve2(input: Input) -> Optional[str]:
+    return None
+
+
+# ==== Solutions with test data ==== #
+
+
+test_data1 = """"""
+test_answer1 = 0
+
+test_data2 = test_data1
+test_answer2 = 0
+
+solves = [
+    {'func': solve1, 'parse': parse_input1,
+        'test_data': test_data1, 'test_answer': test_answer1},
+    {'func': solve2, 'parse': parse_input2,
+        'test_data': test_data2, 'test_answer': test_answer2},
+]
+
+# ==== Template for running solutions ==== #
+
+
+if __name__ == '__main__':
+    filename = 'input.txt'
+    with open(filename, 'r') as f:
+        input = f.read()
+
+        number = 1
+        for solve in solves:
+            func = solve['func']
+            parse = solve['parse']
+
+            slv = func(parse(solve['test_data']))
+            answer = solve['test_answer']
+            if slv == answer:
+                print(f'Solution {number} - Test has passed')
+            else:
+                print(
+                    f'Solution {number} - Test has failed. Should be:\n{answer}\nGot:\n{slv}')
+                number += 1
+                continue
+
+            slv = func(parse(input))
+            if slv is not None:
+                print(f'Solution {number} - The answer:\n{slv}')
+            number += 1
