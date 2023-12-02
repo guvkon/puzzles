9bccf9a93393bf97073c472d38f717ee7d180994
Konstantin Gusev <guvkon.net@icloud.com>
2023-12-02 13:11:36 +0400

    feat(aoc.2023.day2): add solution

---
+++ adventofcode/2023/day2/input.sh
@@ -0,0 +1,3 @@
+#!/bin/zsh
+
+aoc -y 2023 -d 2 d -i input.txt --input-only --overwrite
---
+++ adventofcode/2023/day2/solution.py
@@ -0,0 +1,99 @@
+from __future__ import annotations
+
+import numpy as np
+import re
+import sys
+from dataclasses import dataclass
+from enum import Enum
+from typing import List, Optional, Tuple, Union, Dict, Set, Callable
+
+
+# === Useful Functions === #
+
+
+def splitlines(data: str, fun=lambda x: x) -> List[str]:
+    return [fun(line) for line in data.splitlines() if line]
+
+
+# === Types === #
+
+
+@dataclass
+class Input:
+    lines: List[str]
+
+
+# === Input parsing === #
+
+
+def parse_input(data: str, options: dict) -> Input:
+    lines = splitlines(data)
+    return Input(lines)
+
+
+def parse_input1(data: str) -> Input:
+    return parse_input(data, options={})
+
+
+def parse_input2(data: str) -> Input:
+    return parse_input(data, options={})
+
+
+# === Solutions === #
+
+
+def solve1(input: Input) -> Optional[int]:
+    return None
+
+
+def solve2(input: Input) -> Optional[int]:
+    return None
+
+
+# ==== Solutions with test data ==== #
+
+
+test_data1 = """Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"""
+test_answer1 = 8
+
+test_data2 = test_data1
+test_answer2 = 0
+
+solves = [
+    {'func': solve1, 'parse': parse_input1,
+        'test_data': test_data1, 'test_answer': test_answer1},
+    {'func': solve2, 'parse': parse_input2,
+        'test_data': test_data2, 'test_answer': test_answer2},
+]
+
+# ==== Template for running solutions ==== #
+
+
+if __name__ == '__main__':
+    filename = 'input.txt'
+    with open(filename, 'r') as f:
+        input = f.read()
+
+        number = 1
+        for solve in solves:
+            func = solve['func']
+            parse = solve['parse']
+
+            slv = func(parse(solve['test_data']))
+            answer = solve['test_answer']
+            if slv == answer:
+                print(f'Solution {number} - Test has passed')
+            else:
+                print(
+                    f'Solution {number} - Test has failed. Should be:\n{answer}\nGot:\n{slv}')
+                number += 1
+                continue
+
+            slv = func(parse(input))
+            if slv is not None:
+                print(f'Solution {number} - The answer:\n{slv}')
+            number += 1
