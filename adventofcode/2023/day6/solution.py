ab27ceb95de10cdd9553a50269324a71f6e9196f
Konstantin Gusev <guvkon.net@icloud.com>
2023-12-05 15:55:00 +0400

    chore(aoc.2023.day6): prepare template

---
+++ adventofcode/2023/day6/solution.py
@@ -0,0 +1,119 @@
+from __future__ import annotations
+
+import numpy as np
+import re
+import sys
+from dataclasses import dataclass
+from enum import Enum
+from typing import List, Optional, Tuple, Union, Dict, Set, Callable
+from time import time_ns
+from functools import wraps, cache, cached_property
+from copy import copy
+
+
+# === Useful Functions === #
+
+
+def splitlines(data: str, fun=lambda x: x) -> List[str]:
+    return [fun(line) for line in data.splitlines() if line]
+
+
+def timer(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        start = time_ns()
+        result = f(*args, **kwargs)
+        delta = (time_ns() - start) / 1000000.0
+        print(f'Elapsed time of {f.__name__}: {delta} ms')
+        return result
+
+    return wrapper
+
+
+# === Types === #
+
+
+@dataclass
+class Input:
+    lines: List[str]
+
+
+# === Input parsing === #
+
+
+@timer
+def parse_input(data: str, options: dict) -> Input:
+    lines = splitlines(data)
+    return Input(lines)
+
+
+def parse_input1(data: str) -> Input:
+    return parse_input(data, options={})
+
+
+def parse_input2(data: str) -> Input:
+    return parse_input(data, options={})
+
+
+# === Solutions === #
+
+
+@timer
+def solve1(input: Input) -> Optional[int]:
+    pass
+
+
+@timer
+def solve2(input: Input) -> Optional[int]:
+    pass
+
+
+# ==== Solutions with test data ==== #
+
+
+test_data1 = """"""
+test_answer1 = 0
+
+test_data2 = test_data1
+test_answer2 = 0
+
+solves = [
+    {'func': solve1, 'parse': parse_input1,
+     'test_data': test_data1, 'test_answer': test_answer1},
+    {'func': solve2, 'parse': parse_input2,
+     'test_data': test_data2, 'test_answer': test_answer2},
+]
+
+
+# ==== Template for running solutions ==== #
+
+
+@timer
+def main():
+    filename = 'input.txt'
+    with open(filename, 'r') as f:
+        input = f.read()
+
+        number = 1
+        for solve in solves:
+            func = solve['func']
+            parse = solve['parse']
+
+            slv = func(parse(solve['test_data']))
+            answer = solve['test_answer']
+            if slv == answer:
+                print(f'Solution {number} - Test has passed')
+            else:
+                print(
+                    f'Solution {number} - Test has failed. Should be:\n{answer}\nGot:\n{slv}')
+                number += 1
+                continue
+
+            slv = func(parse(input))
+            if slv is not None:
+                print(f'Solution {number} - The answer:\n{slv}')
+            number += 1
+
+
+if __name__ == '__main__':
+    main()
