7636ed47e9d2adfee1db2317e8cf12d582f9e601
Konstantin Gusev <guvkon.net@icloud.com>
2023-12-07 12:36:00 +0400

    refactor(aoc.2023.day8): prepare improved template

--- adventofcode/2023/day8/input.sh
+++ adventofcode/2023/day8/input.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 YEAR=2023
-DAY=7
+DAY=8
 
 aoc -y $YEAR -d $DAY d -i input.txt --input-only --overwrite
 aoc read -y $YEAR -d $DAY
---
+++ adventofcode/2023/day8/solution.py
@@ -0,0 +1,112 @@
+from __future__ import annotations
+
+import numpy as np
+import re
+import sys
+from dataclasses import dataclass
+from enum import Enum
+from typing import List, Optional, Tuple, Union, Dict, Set, Callable
+from time import time_ns
+from functools import wraps, cache, cached_property, cmp_to_key
+from copy import copy
+
+
+# === Useful Functions === #
+
+
+def splitlines(data: str, fun=lambda x: x) -> List[str]:
+    return [fun(line) for line in data.splitlines() if line]
+
+
+def timer(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        start = time_ns()
+        result = f(*args, **kwargs)
+        delta = (time_ns() - start) / 1000000.0
+        print(f'Elapsed time of {f.__name__}: {delta} ms')
+        return result
+
+    return wrapper
+
+
+# === Types === #
+
+
+@dataclass
+class Input:
+    lines: List[str]
+
+
+# === Input parsing === #
+
+
+@timer
+def parse_input(data: str, options: dict) -> Input:
+    lines = splitlines(data)
+
+    return Input(lines)
+
+
+def parse_input_1(data: str) -> Input:
+    return parse_input(data, options={})
+
+
+def parse_input_2(data: str) -> Input:
+    return parse_input(data, options={})
+
+
+# === Solutions === #
+
+
+@timer
+def solve_1(input: Input) -> Optional[int]:
+    pass
+
+
+@timer
+def solve_2(input: Input) -> Optional[int]:
+    pass
+
+
+# ==== Solutions with test data ==== #
+
+
+test_data_1 = """"""
+test_answer_1 = 1
+
+test_data_2 = test_data_1
+test_answer_2 = 2
+
+
+# ==== Template for running solutions ==== #
+
+
+@timer
+def main():
+    start = time_ns()
+    filename = 'input.txt'
+    with open(filename, 'r') as f:
+        input = f.read()
+        delta = (time_ns() - start) / 1000000.0
+        print(f'Elapsed time of reading file: {delta} ms')
+
+        for number in range(1, 3):
+            func = globals()[f'solve_{number}']
+            parse = globals()[f'parse_input_{number}']
+
+            slv = func(parse(globals()[f'test_data_{number}']))
+            answer = globals()[f'test_answer_{number}']
+            if slv == answer:
+                print(f'\nSolution {number} - Test has passed\n')
+            else:
+                print(f'\nSolution {number} - Test has failed.\nCorrect: {answer}\nBut got: {slv}\n')
+                continue
+
+            slv = func(parse(input))
+            if slv is not None:
+                print(f'\nSolution {number} - The answer is {slv}\n')
+
+
+if __name__ == '__main__':
+    main()
