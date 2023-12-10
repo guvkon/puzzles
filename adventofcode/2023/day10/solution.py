c334bbc187a4b4ae692eaa47de07614e946a4500
Konstantin Gusev <guvkon.net@icloud.com>
2023-12-11 00:17:57 +0400

    feat(aoc.2023.day10): parse input

--- adventofcode/2023/day10/solution.py
+++ adventofcode/2023/day10/solution.py
@@ -34,9 +34,62 @@ def timer(f):
 # === Types === #
 
 
+class NodeType(Enum):
+    top_bottom = '|'
+    left_right = '-'
+    top_right = 'L'
+    top_left = 'J'
+    bottom_left = '7'
+    bottom_right = 'F'
+    ground = '.'
+    start = 'S'
+
+
+class Node:
+    type: NodeType
+    x: int
+    y: int
+
+    def __init__(self, type: NodeType, x: int, y: int):
+        self.type = type
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return f'{self.type} ({self.x}, {self.y})'
+
+    @property
+    def pos(self) -> Tuple[int, int]:
+        return self.x, self.y
+
+    @property
+    def top(self) -> bool:
+        return self.type in [NodeType.top_left, NodeType.top_right, NodeType.top_bottom, NodeType.start]
+
+    @property
+    def bottom(self) -> bool:
+        return self.type in [NodeType.bottom_right, NodeType.bottom_left, NodeType.top_bottom, NodeType.start]
+
+    @property
+    def left(self) -> bool:
+        return self.type in [NodeType.left_right, NodeType.bottom_left, NodeType.top_left, NodeType.start]
+
+    @property
+    def right(self) -> bool:
+        return self.type in [NodeType.left_right, NodeType.bottom_right, NodeType.top_right, NodeType.start]
+
+    @property
+    def is_start(self):
+        return self.type == NodeType.start
+
+
 @dataclass
 class Input:
+    data: str
     lines: List[str]
+    nodes: Dict[Tuple[int, int], Node]
+    start: Node
+    edges: Dict[Tuple[int, int], List[Node]]
 
 
 # === Input parsing === #
@@ -45,7 +98,34 @@ class Input:
 @timer
 def parse_input(data: str, options: dict) -> Input:
     lines = splitlines(data)
-    return Input(lines)
+
+    nodes = {}
+    edges = {}
+    start = None
+    # Build nodes, init edges, and find start.
+    for x in range(0, len(lines[0])):
+        for y in range(len(lines)):
+            node = Node(NodeType(lines[y][x]), x, y)
+            nodes[node.pos] = node
+            edges[node.pos] = []
+            if node.is_start:
+                start = node
+    # Build edges.
+    for node in nodes.values():
+        x, y = node.pos
+        if node.right:
+            right = nodes.get((x + 1, y))
+            if right and right.left:
+                edges[node.pos].append(right)
+                edges[right.pos].append(node)
+        if node.bottom:
+            bottom = nodes.get((x, y + 1))
+            if bottom and bottom.top:
+                edges[node.pos].append(bottom)
+                edges[bottom.pos].append(node)
+    if not start:
+        raise ValueError('Cannot find start node')
+    return Input(data, lines, nodes, start, edges)
 
 
 def parse_input_1(data: str) -> Input:
@@ -59,8 +139,35 @@ def parse_input_2(data: str) -> Input:
 # === Solutions === #
 
 
+def generate_starts(start: Node, connected_nodes: List[Node]) -> List[Node]:
+    edges = {}
+    for node in connected_nodes:
+        edges[node.pos] = node
+    x, y = start.pos
+    left = edges.get((x - 1, y))
+    right = edges.get((x + 1, y))
+    top = edges.get((x, y - 1))
+    bottom = edges.get((x, y + 1))
+    if top and bottom:
+        yield Node(NodeType.top_bottom, x, y)
+    if top and left:
+        yield Node(NodeType.top_left, x, y)
+    if top and bottom:
+        yield Node(NodeType.top_bottom, x, y)
+    if left and bottom:
+        yield Node(NodeType.bottom_left, x, y)
+    if left and right:
+        yield Node(NodeType.left_right, x, y)
+    if bottom and right:
+        yield Node(NodeType.bottom_right, x, y)
+
+
 @timer
 def solve_1(input: Input) -> Optional[int]:
+    nodes = input.nodes
+    edges = input.edges
+    for start in generate_starts(input.start, edges[input.start.pos]):
+        print(start)
     return
 
 
@@ -72,8 +179,13 @@ def solve_2(input: Input) -> Optional[int]:
 # ==== Solutions with test data ==== #
 
 
-test_data_1 = """"""
-test_answer_1 = 0
+test_data_1 = """..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ...
+"""
+test_answer_1 = 8
 
 test_data_2 = test_data_1
 test_answer_2 = 0
