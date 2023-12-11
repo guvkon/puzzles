f3893fe70d995df0d7ce7428c8f4511991f4564b
Konstantin Gusev <guvkon.net@icloud.com>
2023-12-11 17:37:59 +0400

    feat(aoc.2023.day11): add solution for part 2

--- adventofcode/2023/day11/solution.py
+++ adventofcode/2023/day11/solution.py
@@ -45,37 +45,48 @@ class Input:
 # === Input parsing === #
 
 
-def is_dots(lines: List[str], x: int) -> bool:
+def has_galaxies(lines: List[str], x: int) -> bool:
     for y in range(0, len(lines)):
-        if lines[y][x] != '.':
-            return False
-    return True
+        point = lines[y][x]
+        if point == '#':
+            return True
+    return False
 
 
-@timer
-def parse_input(data: str, options: dict) -> Input:
-    lines = splitlines(data)
-
-    # Expand Universe.
+def expand_universe(lines: List[str]) -> List[str]:
     expanded = []
     empty_line = '.' * len(lines[0])
+    expanded_line = 'E' * len(lines[0])
     for line in lines:
-        expanded.append(line)
         if line == empty_line:
+            expanded.append(expanded_line)
+        else:
             expanded.append(line)
     for x in range(len(lines[0]) - 1, -1, -1):
-        if not is_dots(lines, x):
+        if has_galaxies(lines, x):
             continue
         for y in range(0, len(expanded)):
-            expanded[y] = expanded[y][0:x] + '.' + expanded[y][x:]
+            point = 'E'
+            expanded[y] = expanded[y][0:x] + point + expanded[y][x + 1:]
+    return expanded
 
-    # Find galaxies.
+
+def find_galaxies(expanded: List[str]) -> List[Tuple[int, int]]:
     galaxies = []
     for y in range(0, len(expanded)):
         line = expanded[y]
         for x in range(0, len(line)):
             if line[x] == '#':
                 galaxies.append((x, y))
+    return galaxies
+
+
+@timer
+def parse_input(data: str, options: dict) -> Input:
+    lines = splitlines(data)
+
+    expanded = expand_universe(lines)
+    galaxies = find_galaxies(expanded)
 
     return Input(data, lines, expanded, galaxies)
 
@@ -91,21 +102,36 @@ def parse_input_2(data: str) -> Input:
 # === Solutions === #
 
 
-@timer
-def solve_1(input: Input) -> Optional[int]:
+def calculate_distances(lines: List[str], galaxies: List[Tuple[int, int]], amount: int) -> int:
     distances = []
-    galaxies = input.galaxies
     for main_idx in range(0, len(galaxies)):
         for pair_idx in range(main_idx + 1, len(galaxies)):
             xa, ya = galaxies[main_idx]
             xb, yb = galaxies[pair_idx]
-            distances.append(abs(xa - xb) + abs(ya - yb))
+
+            xd = 0
+            for x in range(min(xa, xb), max(xa, xb)):
+                y = min(ya, yb)
+                point = lines[y][x]
+                xd += amount if point in ['E', '='] else 1
+
+            yd = 0
+            for y in range(min(ya, yb), max(ya, yb)):
+                x = max(xa, xb)
+                point = lines[y][x]
+                yd += amount if point in ['E', '|'] else 1
+            distances.append(xd + yd)
     return sum(distances)
 
 
 @timer
+def solve_1(input: Input) -> Optional[int]:
+    return calculate_distances(input.expanded, input.galaxies, 2)
+
+
+@timer
 def solve_2(input: Input) -> Optional[int]:
-    return
+    return calculate_distances(input.expanded, input.galaxies, 1000000)
 
 
 # ==== Solutions with test data ==== #
@@ -125,7 +151,7 @@ test_data_1 = """...#......
 test_answer_1 = 374
 
 test_data_2 = test_data_1
-test_answer_2 = 0
+test_answer_2 = 82000210
 
 
 # ==== Template for running solutions ==== #
