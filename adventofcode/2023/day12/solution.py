bcf5990699e2578768836c9ed6043c052f59416a
Konstantin Gusev <guvkon.net@icloud.com>
2023-12-14 00:59:09 +0400

    wip(aoc.2023.day12): implement solution that passes test of part 1

--- adventofcode/2023/day12/solution.py
+++ adventofcode/2023/day12/solution.py
@@ -47,6 +47,16 @@ class Row:
 
 
 @dataclass
+class Pattern:
+    left: str
+    right: Optional[str]
+
+    @property
+    def wiggle(self) -> bool:
+        return self.right is not None
+
+
+@dataclass
 class Input:
     data: str
     lines: List[str]
@@ -78,20 +88,92 @@ def parse_input_2(data: str) -> Input:
 # === Solutions === #
 
 
+def count_potential_permutations(springs: str) -> int:
+    exp = 0
+    for s in springs:
+        if s == '?':
+            exp += 1
+    return pow(2, exp)
+
+
+start_left = r'[\.\?]*?'
+start_right = r'[\.\?]*'
+between_left = r'[\.\?]+?'
+between_right = r'[\.\?]+'
+end = r'[\.\?]*$'
+
+
+def damaged(length: int) -> str:
+    return r'([#\?]{' + str(length) + r'})'
+
+
+def between(length: int) -> str:
+    return r'[\.\?]{' + str(length) + r'}'
+
+
 def count_arrangements(row: Row) -> int:
-    regex = []
-    for d in row.damaged:
-        regex.append(r'([\?#]{3})')
-    return 0
+    patterns = [Pattern(start_left, start_right)]
+    # Example: r'^[\.\?]*?([#\?]{1})[\.\?]+?([#\?]{6})[\.\?]+([#\?]{5})[\.\?]*$'
+    for count, d in enumerate(row.damaged):
+        patterns.append(Pattern(damaged(d), None))
+        if count < len(row.damaged) - 1:
+            patterns.append(Pattern(between_left, between_right))
+    patterns.append(Pattern(end, None))
+
+    def patterns_step(left_regex: List[str], group: int) -> int:
+        print(f'Left regex = {left_regex}')
+        print(f'Group = {group}')
+        if len(left_regex) == len(patterns):
+            print('Patterns matched the length of left_regex')
+            return 1
+
+        if group > len(row.damaged):
+            print('Group exceeded number of damaged')
+            return 1
+
+        index = len(left_regex)
+        pattern = patterns[index]
+        if not pattern.wiggle:
+            new_regex = [r for r in left_regex]
+            new_regex.append(pattern.left)
+            return patterns_step(new_regex, group + 1)
+
+        before = ''.join(left_regex)
+        remaining = ''.join([patterns[idx].left for idx in range(index + 1, len(patterns))])
+        _left_regex = f'{before}({pattern.left}){remaining}'
+        left_match = re.match(_left_regex, row.springs)
+        left = left_match.start(group + 1)
+        vary_start = len(left_match.group(group))
+        _right_regex = before + pattern.right + remaining
+        right = re.match(_right_regex, row.springs).start(group)
+        print(f'Left = {left}, right = {right}, vary_start = {vary_start}')
+
+        if left == right:
+            new_regex = [r for r in left_regex]
+            new_regex.append(pattern.left)
+            return patterns_step(new_regex, group)
+
+        count = 0
+        for vary in range(vary_start, vary_start + right - left + 1):
+            new_regex = [r for r in left_regex]
+            new_regex.append(between(vary))
+            count += patterns_step(new_regex, group)
+        return count
+
+    return patterns_step([], 1)
 
 
 @timer
 def solve_1(input: Input) -> Optional[int]:
     sum = 0
+    print()
     for row in input.rows:
         print(row)
-        sum += count_arrangements(row)
-    return
+        arrangements = count_arrangements(row)
+        print(f'Arrangements = {arrangements}\n')
+        sum += arrangements
+    print()
+    return sum
 
 
 @timer
